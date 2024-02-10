4c548065b23646a5f4dd921fe5dea2b66d6b45bc
Sajjad Hoviegar <sajjad@Sajjads-MBP.homenet.telecomitalia.it>
2024-02-11 00:01:39 +0100

    adding projects in a static way

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -9,15 +9,15 @@ const projects = [
     name: "Zhuan Restaurant",
     description:
       "A single page application built for a restaurant which contains menu, about page, manage page and so on. The admin can add, edit or delete menu items freely. Normal users (or visitors) can only veiw items and they are able to add, delete or change items in their order list. Any user can sign up and login but only the predefined admin can manage the items. This website offers two different languages with two different layouts (English with LTR layout & Persian with RTL layout).",
-    image: "/thankfulthoughts.png",
+    image: "/zhuan.png",
     github: "https://github.com/s-hoviegar/zhuan-restaurant",
     link: "https://zhuan-restaurant.netlify.app",
   },
   {
-    name: "Offline hash-based password manager",
+    name: "Password Manager",
     description:
       "An offline hash-based password manager which doesn't store any password on any cloud storage or remote server.",
-    image: "/thankfulthoughts.png",
+    image: "/password.png",
     github: "https://github.com/s-hoviegar/hash-based-password-manager",
     link: "https://hash-based-password-manager.netlify.app",
   },
@@ -25,7 +25,7 @@ const projects = [
     name: "Cute Planner",
     description:
       "This react app is a simple project which mixes two different applications in one: a calender and a todo list (or reminder). With the help of this app you can easily add new tasks and track the time remaining for any particular task. This project is a simple CRUD web application that uses firebase realtime databases and firebase authentication for users login and signup.",
-    image: "/thankfulthoughts.png",
+    image: "/cute.png",
     github: "https://github.com/s-hoviegar/cute-planner",
     link: "https://cute-planner.netlify.app",
   },
@@ -33,15 +33,15 @@ const projects = [
     name: "Subtraction Game",
     description:
       'The Subtraction Game (One-Heap Game of Nim) is a mathematical turn-based strategy game that involves removing objects from a set. The objective of the game is to be the player who doesn\'t take the last object. Players take turns removing objects, but they must remove at least one object and at max the number defined in "New Game" page with "Moves" field. The player who takes the last object, loses the game. So be carefull choosing the objects and make your opponent take the last one! Be aware that you cannot beat the computer if the first turn is set to "Computer".',
-    image: "/thankfulthoughts.png",
+    image: "/sub.png",
     github: "https://github.com/s-hoviegar/subtraction-game",
     link: "https://subtraction-game.netlify.app",
   },
   {
-    name: "Conway Game Of Life ⠠⠵",
+    name: "Game Of Life ⠠⠵",
     description:
       "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970. It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves. It is Turing complete and can simulate a universal constructor or any other Turing machine.",
-    image: "/thankfulthoughts.png",
+    image: "/life.png",
     github: "https://github.com/s-hoviegar/conway-game-of-life",
     link: "https://game-of-life-conway-reactjs.netlify.app/",
   },
@@ -72,11 +72,14 @@ const ProjectsSection = () => {
                       />
                     </Link>
                   </div>
-                  <div className="mt-8 md:w-1/2">
+                  <div className="mt-8 md:w-1/2 ">
                     <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
-                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                      {project.description}
-                    </p>
+                    <div className="sm:line-clamp-3 md:line-clamp-1 lg:line-clamp-3">
+                      <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400 ">
+                        {project.description}
+                      </p>
+                    </div>
+                    <br />
                     <div className="flex flex-row align-bottom space-x-4">
                       <Link href={project.github} target="_blank">
                         <BsGithub
