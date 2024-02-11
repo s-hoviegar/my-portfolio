ffd4440038ff48cf233a0f5f18132ad9d08f2e37
Sajjad Hoviegar <sajjad@Sajjads-MBP.homenet.telecomitalia.it>
2024-02-11 01:56:48 +0100

    resolving warnings

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,9 @@
 import React from "react";
 import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
-import { Link } from "react-scroll/modules";
-import { HiArrowUp } from "react-icons/hi";
 
 const Footer = () => {
   return (
     <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="home"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          <HiArrowUp size={35} className="animate-bounce cursor-pointer" />
-        </Link>
-      </div>
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
