392adc2f6b55a4b560da897006c4ed53a4d13122
Sajjad Hoviegar <sajjad@Sajjads-MBP.homenet.telecomitalia.it>
2024-02-11 02:21:57 +0100

    resolving warnings

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -43,18 +43,6 @@ const HeroSection = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-row items-center text-center justify-center ">
-        <Link
-          to="about"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-        >
-          <HiArrowDown size={35} className="animate-bounce cursor-pointer" />
-        </Link>
-      </div>
     </section>
   );
 };
