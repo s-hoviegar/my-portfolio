410f3e6cbaf8757872a5f4e64a6dffb5391a6181
Sajjad Hoviegar <sajjad@Sajjads-MBP.homenet.telecomitalia.it>
2024-02-14 13:44:27 +0100

    adding fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-"use client"
-import "../styles/globals.css"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import { ThemeProvider } from "next-themes"
+"use client";
+import Head from "next/head";
+import "../styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -15,7 +16,31 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <title>Sajjad Hoviegar</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className="dark:bg-stone-900">
         <ThemeProvider enableSystem={true} forcedTheme="dark" attribute="class">
           <Navbar />
@@ -24,5 +49,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
